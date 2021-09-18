0756cd969a05ca81b3c9ea55a4ad421bc67f08ea
Anastasia-Andruhovich <anastasia.andruhovich@ibm.com>
2021-09-18 12:36:56 +0300

    Initial commit

---
+++ src/main/java/by/andruhovich/recepies/RecepiesApplication.java
@@ -0,0 +1,13 @@
+package by.andruhovich.recepies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepiesApplication.class, args);
+	}
+
+}
